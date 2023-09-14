e086929d4ceab76d4857ca98e36a3433ec046d2c
adilaziz986 <adilaziz986@gmail.com>
2023-09-14 14:33:55 -0700

    first commit

---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/question_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 150, 3, 170),
+              Color.fromARGB(255, 62, 0, 138),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: activeScreen,
+      ),
+    );
+  }
+}
---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final Function() startQuiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 150, 3, 170),
+                Color.fromARGB(255, 62, 0, 138),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    "assets/images/quiz-logo.png",
+                    color: const Color.fromARGB(150, 255, 255, 255),
+                    width: 300,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text(
+                    "Learn flutter in fun way!",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Color.fromARGB(150, 255, 255, 255)),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  OutlinedButton.icon(
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: startQuiz,
+                    icon: const Icon(
+                      Icons.arrow_right_alt,
+                    ),
+                    label: const Text(
+                      "Start Quiz!",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
