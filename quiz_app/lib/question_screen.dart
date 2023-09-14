afd9d803925acc3935dd3b197e8ca3b291ceaecc
adilaziz986 <adilaziz986@gmail.com>
2023-09-14 15:57:53 -0700

    67 completed

--- quiz_app/lib/question_screen.dart
+++ quiz_app/lib/question_screen.dart
@@ -10,6 +10,29 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Material();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("..."),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Answer1"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Answer2"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Answer3"),
+          ),
+        ],
+      ),
+    );
   }
 }
